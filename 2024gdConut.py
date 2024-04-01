ad5fc33ebcda007034d033651c9706b1597af1f2
mazewei <978735584@qq.com>
2024-04-01 23:31:00 +0800

    最后的调整

--- 2024gdConut.py
+++ 2024gdConut.py
@@ -22,25 +22,7 @@ for index in range(21):
         }
 
         headers = {
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Accept-Language": "zh-CN,zh;q=0.9",
-            "Cache-Control": "no-cache",
-            "Connection": "keep-alive",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Cookie": "JSESSIONID=DBKZc-IITBrZ-KgrAdWpYCLIbDrRveZML6M3s7jT6PR_tXFd9yVW!-988052771; _gscu_260182935=05460498k0095j21; _gscbrs_260182935=1; arialoadData=true; ../issoYH_MASS_TOKEN=7e83beb5-5ddd-4057-961f-7063891e9ec9; YH_MASS_TOKEN=aaadfe06-339f-4f62-9e87-ab7e453ad3b0",
-            "Host": "ggfw.hrss.gd.gov.cn",
-            "Origin": "https://ggfw.hrss.gd.gov.cn",
-            "Pragma": "no-cache",
-            "Referer": "https://ggfw.hrss.gd.gov.cn/sydwbk/center.do?nvt=1711973392060",
-            "Sec-Ch-Ua": "\"Not A(Brand\";v=\"99\", \"Google Chrome\";v=\"121\", \"Chromium\";v=\"121\"",
-            "Sec-Ch-Ua-Mobile": "?0",
-            "Sec-Ch-Ua-Platform": "\"Windows\"",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-            "X-Requested-With": "XMLHttpRequest"
+            '你的header'
         }
         resp = requests.post(url=url,headers=headers,data=data,verify=False)
         datas = resp.json()['rows']
